e86d8497fca888f8f74412fefe56b93a8733670f
Andrew Byrnes <byrnes.aw@gmail.com>
2024-04-05 19:12:21 -0500

    creating table

--- app.py
+++ app.py
@@ -11,3 +11,21 @@ def testing():
     conn = psycopg2.connect("postgres://hello_world_postgres_user:thSKOlH19WgLeN8VVIWTwFX9Ss0H6w4e@dpg-co89135jm4es738vll40-a/hello_world_postgres")
     conn.close()
     return "Database Connection Successful" 
+
+@app.route('/db_create')
+def creating():
+    conn=psycopg2.connect("postgres://hello_world_postgres_user:thSKOlH19WgLeN8VVIWTwFX9Ss0H6w4e@dpg-co89135jm4es738vll40-a/hello_world_postgres")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+            );
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
+
